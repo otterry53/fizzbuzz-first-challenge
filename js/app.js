8fb87f8732a1c535272b2bd9d4e10a1fa1608ac8
otterry53 <twlow88@gmail.com>
2016-03-08 16:27:07 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    for (var myNumber = 1; myNumber <= 100; myNumber++) {
+        if (myNumber % 15 === 0) {
+            $('body').append("<br>fizzbuzz");
+        } else if (myNumber % 3 === 0) {
+            $('body').append("<br>fizz");
+        } else if (myNumber % 5 === 0) {
+            $('body').append("<br>buzz");
+        } else {
+            $('body').append("<br>" + myNumber);
+        }
+    }
+});
